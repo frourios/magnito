669d7dab73d6318a5d1dd0ff30ad004905cd0641
solufa <solufa2020@gmail.com>
2026-03-15 22:45:59 +0900

    fix: set cors headers

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,25 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   typescript: { ignoreBuildErrors: true },
   env: { APP_VERSION: `v${packageJson.version}` },
+  headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'amz-sdk-invocation-id,amz-sdk-request,cache-control,content-type,x-amz-target,x-amz-user-agent',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
